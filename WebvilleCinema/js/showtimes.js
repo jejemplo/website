c3bbc74231eb61951e25c57c403f6297aa1ab3a1
Jose Ejemplo <jelkner@yahoo.com>
2024-01-01 10:22:21 -0500

    Finish Webville Cinema

--- WebvilleCinema/js/showtimes.js
+++ WebvilleCinema/js/showtimes.js
@@ -27,22 +27,23 @@ function getTimeFromString(timeString) {
     return theTime.getTime();
 }
 
-function  getNextShowing(movie) {
+function getNextShowing(movie) {
   let now = new Date().getTime();
   for (let i = 0; i < movie.showtimes.length; i++) {
     let showtime = getTimeFromString(movie.showtimes[i]);
     if ((showtime - now) > 0) {
-      return "Next showing of " + movie.title + " is " + movie.showtimes[i];
+      return movie.showtimes[i];
     }
   }
-  return null;
+  return movie.showtimes[0];
 }
 
 function showMovieList() {
   let list = document.getElementById("nowPlaying");
   let s = "<tr><th>Title</th><th>Next Showing</th></tr>"
   for (let i = 0; i < movies.length; i++) {
-    s += "<tr><td>" + movies[i].title + "</td><td></td></tr>";
+    s += "<tr><td>" + movies[i].title + "</td>";
+    s += "<td>" + getNextShowing(movies[i]) + "</td></tr>";
   }
   list.innerHTML = s;
 }
