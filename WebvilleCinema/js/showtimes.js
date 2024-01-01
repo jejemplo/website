1ee49a0e1473fb2bca99aeeeece4116fd01538fb
Jose Ejemplo <jelkner@yahoo.com>
2024-01-01 10:10:47 -0500

    Update Webville Cinema

---
+++ WebvilleCinema/js/showtimes.js
@@ -0,0 +1,48 @@
+movies = [
+  {
+    title: "Plan 9 from Outer Space",
+    genre: "Cult Classic",
+    rating: 5,
+    showtimes: ["3:00pm", "7:00pm", "11:00pm"]
+  },
+  {
+    title: "Forbidden Planet",
+    genre: "Classic Sci-fi",
+    rating: 5,
+    showtimes: ["5:00pm", "9:00pm"]
+  },
+  {
+    title: "Buckaroo Banzai",
+    genre: "Cult Classic",
+    rating: 5,
+    showtimes: ["1:00pm", "5:00pm", "7:00pm"]
+  }
+];
+
+function getTimeFromString(timeString) {
+    let theTime = new Date();
+    let time = timeString.match(/(\d+)(?::(\d\d))?\s*(p?)/);
+    theTime.setHours(parseInt(time[1]) + (time[3] ? 12: 0));
+    theTime.setMinutes(parseInt(time[2]) || 0);
+    return theTime.getTime();
+}
+
+function  getNextShowing(movie) {
+  let now = new Date().getTime();
+  for (let i = 0; i < movie.showtimes.length; i++) {
+    let showtime = getTimeFromString(movie.showtimes[i]);
+    if ((showtime - now) > 0) {
+      return "Next showing of " + movie.title + " is " + movie.showtimes[i];
+    }
+  }
+  return null;
+}
+
+function showMovieList() {
+  let list = document.getElementById("nowPlaying");
+  let s = "<tr><th>Title</th><th>Next Showing</th></tr>"
+  for (let i = 0; i < movies.length; i++) {
+    s += "<tr><td>" + movies[i].title + "</td><td></td></tr>";
+  }
+  list.innerHTML = s;
+}
